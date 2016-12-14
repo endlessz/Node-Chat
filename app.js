f520a5cac5dafb9bceffc7baa94361290af4f85b
Wasut <wasut_pao@hotmail.com>
2016-12-14 21:23:03 +0700

    add connect mongodb, socket insert input to mongo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var client = require('socket.io').listen(server).sockets;
+var mongo = require('mongodb').MongoClient;
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
@@ -12,6 +13,26 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/index.html")
 });
 
-client.on('connection', function(socket){
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
+	if(err){
+		throw err;
+	}
+
+	client.on('connection', function(socket){
 	console.log('Connected...');
+
+		var collection =  db.collection('messages');
+
+		socket.on('input', function(data){
+			var chat = {
+				name: data.name,
+				message: data.message
+			}
+
+			collection.insert(chat, function(){
+				console.log('Insert Success')
+			});
+		});
+	});
+
 });
