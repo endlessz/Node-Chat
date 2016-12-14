b6b9dcb4a1e15024b8885068092d643893b701b0
Wasut <wasut_pao@hotmail.com>
2016-12-14 19:48:31 +0700

    add client socket io, test connect

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var client = require('socket.io').listen(server).sockets;
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
@@ -9,4 +10,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/index.html")
+});
+
+client.on('connection', function(socket){
+	console.log('Connected...');
 });
